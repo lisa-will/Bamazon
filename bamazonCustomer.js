0672f42916f01f0f280df067b07633e2397cc8bd
Lisa Williams <williams.lisarenee@gmail.com>
2016-12-18 14:32:58 -0500

    added cli-table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,9 @@
 "use strict";
 
 var mysql = require("mysql");
-var inquirer = require("inquirer");
-var Table = require("easy-table");
+// var inquirer = require("inquirer");
+// var Table = require("easy-table");
+var Table = require("cli-table");
 
 var ourTable = new Table();
 
@@ -18,50 +19,141 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  //console.log("I am connected");
-});
 
-// Query Database
-function queryDatabase(query, whereClause)
-{
+//Checks connection
+    connection.connect(function(err) {
+        if (err) throw err;
+    });
 
-    //console.log(query);
 
-     connection.query(query, whereClause, function(err, res){
+
+//Runs program
+function start(){
+    var query = "SELECT * FROM product";
+     connection.query(query, function(err, res){
          if(err){
              throw err;
          }
-
-         //console.log(res);
-         displayResult(res);
-       
-         
-    });
+         displayResults(res);
+         promptCustomer();
+});
 }
 
-var displayResult = function(results){
-    for(var i = 0; i < results.length; i++){
-        var record = results[i];
-        ourTable.cell("Product ID: ", record.item_id);
-        ourTable.cell("Product Name: ", record.product_name);
-        // ourTable.cell("Department: ", record.department_name);
-        ourTable.cell("Price (USD): ", record.price, Table.number(2));
-        // ourTable.cell("Qty: ", record.stock_quantity);
-        ourTable.newRow();
+
+
+//Pulls inventory list into pretty table
+//var Table = require("cli-table");
+
+//var ourTable = new Table();
+
+function displayResults(results){
+   var ourTable = new Table({
+       head: ['Item ID', 'Product Name', 'Price (USD)', 'Department'],
+       colAligns: ['left', 'left', 'right', 'left']
+   });
+   for(var i = 0; i < results.length; i++){
+       var record = results[i];
+      // console.log(JSON.parse(JSON.stringify(record)));
+       ourTable.push([record.item_id, record.product_name, record.price.toFixed(2), record.department_name]);
+   }
+   console.log(ourTable.toString());
+};
+
+
+//Puts Customer's purchases into pretty table
+function displayPurchases(results, quantityOrdered, cost){
+   var ourTable = new Table({
+       head: ['Item ID', 'Product Name', 'Price (USD)', 'Quantity', 'Cost'],
+       colAligns: ['left', 'left', 'right', 'right', 'right']
+   });
+   for(var i = 0; i < results.length; i++){
+       var record = results[i];
+      //console.log(JSON.parse(JSON.stringify(record)));
+       ourTable.push([record.item_id, record.product_name, record.price.toFixed(2), quantityOrdered, cost.toFixed(2)]);
+   }
+   console.log(ourTable.toString());
+};
+
+
+
+//Inquire Customer on what they want to buy
+var inquirer = require('inquirer');
+
+function promptCustomer(){
+    inquirer.prompt([
+        //Prompt user on what item they want to purchase
+        {
+            type: "input",
+            message: "Please enter the id for the item you would like to purchase. Or enter Q to quit.",
+            name: "itemToPurchase",
+        },
+
+        //Quantity to be purchased
+        {
+            when: function (response) {
+                        return response.itemToPurchase.toUpperCase() != 'Q';
+                    },
+            type: "input",
+            message: "How many woud you like?",
+            name: "quantityToPurchase"
+        }
+    ]).then(processOrder);
+}
+
+
+    var totalPurchase = 0;
+
+function processOrder(answers){
+
+    if (answers.itemToPurchase.toUpperCase() == "Q")
+    {
+        console.log("Thank you for visiting!");
+        console.log("Your total purchase was: $" + totalPurchase);
+        connection.end();
+        return;
     }
-    console.log(ourTable.toString())
+
+    var queryItem = "SELECT * FROM product WHERE item_id = '" + answers.itemToPurchase + "'";
+    connection.query(queryItem, function(err, res){
+        if(err) {
+            throw err;
+        }
+
+        // Checks to see if it is a valid item
+        if(res.length == 0){
+            console.log("Item " + answers.itemToPurchase + " is not a valid item.");
+            promptCustomer();
+            return;
+        }
+
+        // Checks to see if the stock quantity is more than the requested purchase quantity
+        var quantityOrdered = parseInt(answers.quantityToPurchase);
+        if(quantityOrdered > res[0].stock_quantity){
+            console.log("Sorry, We do not have that many in stock");
+            promptCustomer();
+            return;
+        }
+
+        var cost = quantityOrdered * res[0].price;
+
+        // Displays what they purchased
+        displayPurchases(res, quantityOrdered, cost);
+
+        // console.log("Item(s) cost you $" + cost);
+
+        // Displays the total cost of purchases
+        totalPurchase += cost;
+
+        var newQuantity = res[0].stock_quantity - quantityOrdered;
+        var query = "UPDATE product SET stock_quantity = " + newQuantity + " WHERE item_id = '" + answers.itemToPurchase + "'";
+        connection.query(query, function(err, res){
+            if(err) {
+                throw err;
+            }
+            start();
+        });
+    });
 };
 
-var department = "";
-var whereClause = {
-    //department_name: department
-};
-
-var query = "SELECT * FROM product";
-
-queryDatabase(query, whereClause);
-
-connection.end();
-
+// Calling the function
+start();
